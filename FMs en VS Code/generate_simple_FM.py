c487b715b29c07f06a61e15c908acaf04ef37193
Enriquelp <enriquelopezencinas@uma.es>
2024-10-23 12:09:37 +0200

    Añadida la opcion para simplificar mas o menos el modelo de kubernetes con el script generate_simple_fm.py

--- FMs en VS Code/generate_simple_FM.py
+++ FMs en VS Code/generate_simple_FM.py
@@ -1,11 +1,30 @@
-# script para simplificar y adaptar un modelo de caracteristicas para poder trabajar
-# con la herramienta de flamapy (tambien borra las restricciones)
+# script para simplificar y adaptar un modelo de características para poder trabajar
+# con la herramienta de Flamapy (también borra las restricciones)
 
 import re
 
 fm_original = 'Feature Models\Kubernetes_manifest_v2.uvl'
 fm_simplified = 'Feature Models\Kubernetes_manifest_v2_simplified.uvl'
 
+# Palabras o caracteres a eliminar (comentar las que no se deseen eliminar)
+# words_to_delete = ['String ', 'Integer ', 'Boolean ', 'cardinality', '[1..*]']  
+words_to_delete = [] 
+
+# Caracteres a eliminar de las restricciones (comentar los que no se deseen eliminar)
+# characters_to_delete = [' < ', ' > ', ' <=> ', ' == ', '//']
+characters_to_delete = ['//']
+
+# Función para simplificar las restricciones
+def simplified_constraints(restrictions):
+    # Eliminar las restricciones no soportadas actualmente por Flamapy
+    lines_restrictions = restrictions.split('\n')
+    simplified_restrictions = []
+    for line in lines_restrictions:
+        if any(c in line for c in characters_to_delete):
+            continue
+        else:
+            simplified_restrictions.append(line)
+    return simplified_restrictions
 
 # Función para procesar el texto
 def generate_fm_simplified(fm_original, fm_simplified, palabras_a_eliminar):
@@ -15,34 +34,30 @@ def generate_fm_simplified(fm_original, fm_simplified, palabras_a_eliminar):
 
     # Eliminar y reemplazar las palabras o caracteres especificados
     for palabra in palabras_a_eliminar:
-        text = text.replace(palabra, '')  
+        text = text.replace(palabra, '')
+    
+    # Reemplazar todos los puntos por guiones bajos
     text = text.replace('.', '_')
 
-    # Separ el modelo de caracteristicas en dos partes, las caracteristicas y las restricciones
+    # Reemplazar '[1__*]' por '[1..*]'
+    text = text.replace('[1__*]', '[1..*]')
+    
+    # Separar el modelo de características en dos partes: las características y las restricciones
     tree = re.split(r'^\s*constraints', text, maxsplit=1, flags=re.MULTILINE)[0]
     restrictions = re.split(r'^\s*constraints', text, maxsplit=1, flags=re.MULTILINE)[1]
 
-    # Eliminar las restricciones no soportadas actualmente por flamapy
-    lines_restrictions = restrictions.split('\n')
-    simplified_restrictions = []
-    for line in lines_restrictions:
-        if any(c in line for c in characters_to_delete):
-            continue
-        else:
-            simplified_restrictions.append(line)
+    # Simplificar las restricciones (Comentar esta línea si no se desea simplificar las restricciones)
+    restrictions = simplified_constraints(restrictions)
+    
 
     # Guardar el texto modificado en un nuevo archivo
     with open(fm_simplified, 'w', encoding='utf-8') as f:
         f.write(tree)
         f.write('constraints')
-        for line in simplified_restrictions:
+        for i, line in enumerate(restrictions):
             f.write(line)
-            f.write('\n')
-
-
-# Palabras o caracteres a eliminar
-words_to_delete = ['String ', 'Integer ', 'Boolean ', 'cardinality', '[1..*]']  # Ajusta estas palabras
-characters_to_delete = [' < ', ' > ', ' <=> ', ' == ', '//']
+            if i < len(restrictions) - 1:
+                f.write('\n')
 
 # Llamar a la función para procesar el archivo
 generate_fm_simplified(fm_original, fm_simplified, words_to_delete)
